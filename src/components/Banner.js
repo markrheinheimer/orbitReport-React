f12629801cc258dcc995fcf48ed189d599c8698c
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-07-31 14:15:23 -0500

    added buttons

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+export default function Banner() {
+  return (
+    <>
+      <header>Orbit Reaport</header>
+      <p>Click on the buttons to see the satellites in that orbit type</p>
+    </>
+  );
+}
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,21 @@
-const Buttons = () => {
+import React from 'react';
+
+import satData from './satData';
+import App from '../App';
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+
+      <button onClick={(satData) => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
